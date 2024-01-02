6b0f3643f155152127a6cfc21e88282f81d94649
Tejas013 <tejasmadhukar6@gmail.com>
2024-01-03 03:42:54 +0530

    Added custom mapping

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,22 +5,16 @@ local capabilities = configs.capabilities
 
 local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
--- local servers = { "pyright"}
 
--- for _, lsp in ipairs(servers) do
+-- local default = {"docker_compose_language_service", "dockerls"}
+--
+-- for _, lsp in ipairs(default) do
 --   lspconfig[lsp].setup {
 --     on_attach = on_attach,
 --     capabilities = capabilities,
 --   }
 -- end
 
--- Without the loop, you would have to manually set up each LSP 
--- 
--- lspconfig.html.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
--- }
---
 lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
