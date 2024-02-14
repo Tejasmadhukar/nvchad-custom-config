4a55d961e1fca8eb206f796ed8d90e6adc17d811
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-14 14:59:30 +0530

    Added vim tmux navigation

--- mappings.lua
+++ mappings.lua
@@ -5,7 +5,11 @@ M.general = {
     ["<C-d>"] = {"<C-d>zz", "scrolls half a page down"},
     ["<C-u"] = {"<C-u>zz", "scrolls half a page up"},
     ["n"] = {"nzzzv", "next search element"},
-    ["N"] = {"Nzzzv", "previous search element"}
+    ["N"] = {"Nzzzv", "previous search element"},
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>","window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>","window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>","window down"},
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up"},
   }
 }
 
